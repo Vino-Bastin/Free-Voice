1134df83e03ceb93286ca7024732876c3edb63f2
vinobastin <vino105264@gmail.com>
2023-01-20 08:06:03 +0530

    app created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Router } from "react-router-dom";
+
+import Main from "../pages/Main";
+import SignUp from "../pages/SignUp";
+import Login from "../pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Route path="/" exact component={<Main />} />
+        <Route path="/signup" exact component={<SignUp />} />
+        <Route path="/login" exact component={<Login />} />
+      </Router>
+    </>
   );
 }
 
---
+++ src/firebase.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjXJgMZR5XLXyVY9mfS607cNnolh0ywz4",
+  authDomain: "chat-12033.firebaseapp.com",
+  projectId: "chat-12033",
+  storageBucket: "chat-12033.appspot.com",
+  messagingSenderId: "160126642057",
+  appId: "1:160126642057:web:911d825ed77280e3096358",
+};
+
+export const app = initializeApp(firebaseConfig);
